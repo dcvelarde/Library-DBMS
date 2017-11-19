75f6e0e357956fc411f720b97c92fb92a6408fed
dcvelarde <dvelarde@ryerson.ca>
2017-11-19 15:37:56 -0500

    added start for GUI

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args)throws Exception {
@@ -36,6 +38,28 @@ public class Main {
 		DropTables dt = new DropTables();
 		ct.createTables(s);
 		dt.dropTables(s);
+		
+		String[] buttons = { "Create Tables", "Drop Tables", "Populate Tables", "Queries","Cancel"};
+		String[] buttons2 = { "", "Query1", "Query2", "Query3","Cancel"};
+		int returnValue = 0;
+		String secondReturnValue = "";
+		while(returnValue != 4)
+		{
+			returnValue = JOptionPane.showOptionDialog(null, "Please select one of the following options: ",
+			"Library DBMS", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);
+			if(returnValue == 3)
+			{
+				while(secondReturnValue != null)
+				{
+					secondReturnValue = JOptionPane.showInputDialog("Select a query to execute: \n"
+							+ "1) SELECT * FROM LIBRARY_USERS\n"
+							+ "2) SELECT * FROM ITEMS\n", "text");
+					if(secondReturnValue != null)
+						JOptionPane.showMessageDialog(null, "Results from Query printed here.");
+				}
+			}
+		}
+		
 		s.close();
 		System.out.println("Closing db connection.");
 		connection.close();
---
+++ src/PopulateTables.java
@@ -0,0 +1,21 @@
+import java.sql.Statement;
+
+public class PopulateTables {
+
+	public PopulateTables()
+	{
+		
+	}
+	
+	public void populateTables(Statement statement)
+	{
+		try
+		{
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
