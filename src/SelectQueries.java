43d9724cda503535a01b3b2d4729e934d6c81de7
erickxon <johnquil9@gmail.com>
2017-11-20 15:22:58 -0500

    Update SelectQueries.java

--- src/SelectQueries.java
+++ src/SelectQueries.java
@@ -2,8 +2,8 @@
 public class SelectQueries 
 {
 	public static final String selQuery1="SELECT * FROM LIBRARY_USERS";
-	public static final String selQuery2="";
-	public static final String selQuery3="";
+	public static final String selQuery2="SELECT * FROM ITEMS";
+	public static final String selQuery3="SELECT * FROM STUDY_ROOMS WHERE MAX_CAPACITY >=5";
 	public static final String selQuery4="";
 	public static final String selQuery5="";
 	public static final String selQuery6="";
