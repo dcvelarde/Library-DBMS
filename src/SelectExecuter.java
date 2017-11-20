8fa2a79975741712d0ac91820156cec53339ec7c
dcvelarde <dvelarde@ryerson.ca>
2017-11-19 22:44:48 -0500

    added classes for select queries

---
+++ src/SelectExecuter.java
@@ -0,0 +1,113 @@
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SelectExecuter 
+{
+	
+	public SelectExecuter(){}
+	
+	public String getSelectResults(Statement statement, int option)
+	{
+		StringBuilder sb = new StringBuilder();
+		ResultSet rs = null;
+		try
+		{
+			if(option == 1)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery1);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 2)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery2);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 3)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery3);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 4)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery4);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 5)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery5);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 6)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery6);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 7)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery7);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 8)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery8);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 9)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery9);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else if(option == 10)
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery10);
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+			}
+			else
+			{
+				rs = statement.executeQuery(SelectQueries.selQuery11);				
+				while(rs.next())
+				{
+					//Get values from columns and append it to string builder object
+				}
+				
+			}
+			rs.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
+	}
+}
---
+++ src/SelectQueries.java
@@ -0,0 +1,16 @@
+
+public class SelectQueries 
+{
+	public static final String selQuery1="";
+	public static final String selQuery2="";
+	public static final String selQuery3="";
+	public static final String selQuery4="";
+	public static final String selQuery5="";
+	public static final String selQuery6="";
+	public static final String selQuery7="";
+	public static final String selQuery8="";
+	public static final String selQuery9="";
+	public static final String selQuery10="";
+	public static final String selQuery11="";
+
+}
